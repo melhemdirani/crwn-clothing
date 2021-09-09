befce212159cfdaff0432901fda96436a3c41e73
Melhem El Dirani <melhemdirani@Melhems-MacBook-Air.local>
2021-09-09 12:43:20 +0300

    Updates

---
+++ precache-manifest.f2771fb7d887da1e9f925def673b4a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5091d4e6c66df198e57beda8f23c31a2",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "1ba0cf4c7081cef38871",
+    "url": "/crwn-clothing/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "16501e907e59042658c6",
+    "url": "/crwn-clothing/static/js/2.58fd4c14.chunk.js"
+  },
+  {
+    "revision": "1ba0cf4c7081cef38871",
+    "url": "/crwn-clothing/static/js/main.6c4ac3fe.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
