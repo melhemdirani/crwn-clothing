af6b9cac641194099d77c508c2b5fc7015790b73
Melhem El Dirani <melhemdirani@Melhems-MacBook-Air.local>
2021-09-09 12:55:33 +0300

    minor changes

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51JOjNQEtUVCNTHLn1BpQbo7HC37EhIYzUeLn6RqFzFJDQexKq19KMtVWEQIJmqjqYANwugeTbStI6GXAbX03rul000UZdGZxEl';
 
   const onToken = token => {
-    axios({
-      url: 'payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token: token
-      }
-    })
-      .then(response => {
-        alert('succesful payment');
-      })
-      .catch(error => {
-        console.log('Payment Error: ', JSON.parse(error));
-        alert(
-          'There was an issue with your payment! Please make sure you use the provided credit card.'
-        );
-      });
+    console.log(token);
+    alert('Payment Succesful!');
   };
 
   return (
